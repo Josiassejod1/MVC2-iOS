e43fb96ff9e687de7bc5bb2ba9df7de73d443ac1
dalvin <dalvin.sejour@nbcuni.com>
2019-01-14 11:04:36 -0500

    Initial Commit

---
+++ MarvelVsCapcom2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelVsCapcom2
+//
+//  Created by Dalvin Sejour on 1/14/19.
+//  Copyright © 2019 Dalvin Sejour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
