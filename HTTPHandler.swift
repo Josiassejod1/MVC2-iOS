8ea91bf6f38b897ba3211ba11792ccec06d021df
dalvin <dalvin.sejour@nbcuni.com>
2019-01-14 15:56:31 -0500

    get data to work for view

---
+++ HTTPHandler.swift
@@ -0,0 +1,9 @@
+//
+//  HTTPHandler.swift
+//  MarvelVsCapcom2
+//
+//  Created by Dalvin Sejour on 1/14/19.
+//  Copyright © 2019 Dalvin Sejour. All rights reserved.
+//
+
+import Foundation
---
+++ MarvelVsCapcom2/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  MarvelVsCapcom2
+//
+//  Created by Dalvin Sejour on 1/14/19.
+//  Copyright © 2019 Dalvin Sejour. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
